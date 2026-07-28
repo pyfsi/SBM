7370f147bef9c463f28865ff1682bfe7f8a46209
Ryan Adiputra <ryanadiputra230798@gmail.com>
2026-07-28 15:28:24 +0200

    cleanup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-test=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sbm=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # define path to SBM directory
 echo "Defining paths SBM and SBM_POSTPROC"
-export SBM=$test
-export SBM_POSTPROC=$test/postprocess
+export SBM=$sbm
+export SBM_POSTPROC=$sbm/postprocess
 
 # append SBM directory path to PYTHONPATH
-echo "Adding $test to PYTHONPATH"
-export PYTHONPATH=$test:$PYTHONPATH
+echo "Adding $sbm to PYTHONPATH"
+export PYTHONPATH=$sbm:$PYTHONPATH
