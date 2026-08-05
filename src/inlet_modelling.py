8144627b38e8e46404e86817d9e3598905c82191
Ryan Adiputra <ryanadiputra230798@gmail.com>
2026-08-05 13:02:10 +0200

    fix print msg

--- src/inlet_modelling.py
+++ src/inlet_modelling.py
@@ -216,7 +216,7 @@ def inlet_modelling(config):
     n_time_insert = int((t_end-t_start)/dt_insert) + 1
     logger.info(f"Between t_start {t_start} s and t_end {t_end} s, {n_time_insert} intervals of {dt_insert} s need to be defined.")
     for t_insert_idx in range(n_time_insert - 1): # no insertion at last time step
-        start_msg = f"Start bubble calculation for time interval {t}"
+        start_msg = f"Start bubble calculation for time interval {t_insert_idx}"
         logger.info(start_msg)
         print(start_msg)
         mg_inserted = inlet_model.insert_bubbles(t_insert_idx)
